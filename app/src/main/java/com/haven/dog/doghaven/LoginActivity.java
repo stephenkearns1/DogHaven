f64558df0fcba8f1d62d11e7d0752c15ad261cb6
stephenkearns1 <bohsbabycrew1@gmail.com>
2016-10-25 10:37:32 +0100

    Setting up the project

---
+++ app/src/main/java/com/haven/dog/doghaven/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.haven.dog.doghaven;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
