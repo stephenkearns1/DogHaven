de92a88340ab34c41f2a2efb83b19380c5d38448
barry lougheed <barrylougheed43@hotmail.com>
2016-11-10 23:53:57 +0000

    pushing activities

---
+++ app/src/main/java/com/haven/dog/doghaven/Activities/CompletedDogMatch.java
@@ -0,0 +1,31 @@
+package com.haven.dog.doghaven.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.haven.dog.doghaven.R;
+
+public class CompletedDogMatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_completed_dog_match);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
