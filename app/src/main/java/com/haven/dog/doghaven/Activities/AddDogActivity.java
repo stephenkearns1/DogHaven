0c58dac945df05dcc9f13354d90b196eaebeb150
MarkenTeder <marken.teder@gmail.com>
2016-11-10 21:18:48 +0000

    Finished AddDog Page

--- app/src/main/java/com/haven/dog/doghaven/Activities/AddDogActivity.java
+++ app/src/main/java/com/haven/dog/doghaven/Activities/AddDogActivity.java
@@ -1,6 +1,10 @@
 package com.haven.dog.doghaven.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
 import com.haven.dog.doghaven.R;
 
 /**
@@ -8,9 +12,33 @@ import com.haven.dog.doghaven.R;
  */
 
 public class AddDogActivity extends AppCompatActivity {
+
+    Spinner dropdown;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_dog_page);
+        dropdown=(Spinner) findViewById(R.id.dropDown);
+        addListenerOnDropDown();
+
+
+
+
     }
+
+    private void addListenerOnDropDown() {
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
 }
