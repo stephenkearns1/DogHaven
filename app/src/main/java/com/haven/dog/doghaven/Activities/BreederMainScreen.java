4ec30c512cd70fb1e15977507e1413a6e94d0942
seanbrady180 <seanbrady180@gmail.com>
2016-11-11 04:49:56 +0000

    BreederMain Menu Link

--- app/src/main/java/com/haven/dog/doghaven/Activities/BreederMainScreen.java
+++ app/src/main/java/com/haven/dog/doghaven/Activities/BreederMainScreen.java
@@ -1,15 +1,18 @@
 package com.haven.dog.doghaven.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.haven.dog.doghaven.R;
 
-public class BreederMainScreen extends AppCompatActivity {
+public class BreederMainScreen extends AppCompatActivity implements View.OnClickListener {
+    Button profile,stud,add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +20,38 @@ public class BreederMainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_breeder_main_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        profile=(Button) findViewById(R.id.profileBtn);
+        stud= (Button) findViewById(R.id.studMatch);
+        add=(Button) findViewById(R.id.addDog);
+        profile.setOnClickListener(this);
+        stud.setOnClickListener(this);
+        add.setOnClickListener(this);
 
 
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.profileBtn:
+               Intent pb = new Intent(this,CompanyProfile.class);
+                startActivity(pb);
+                break;
+            case R.id.studMatch:
+                Intent info= new Intent(this,StudMatchActivity.class);
+                startActivity(info);
+                break;
+
+            case R.id.addDog:
+                Intent ad= new Intent(this,AddDogActivity.class);
+                startActivity(ad);
+
+                break;
+
+
+
+        }
     }
 
 }
