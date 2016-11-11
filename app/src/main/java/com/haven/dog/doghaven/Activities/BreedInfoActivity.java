8aeec08d9dbf792bb1ae05d4d5380633d952907c
MarkenTeder <marken.teder@gmail.com>
2016-11-11 03:53:13 +0000

    Stud Match Page Complete

--- app/src/main/java/com/haven/dog/doghaven/Activities/BreedInfoActivity.java
+++ app/src/main/java/com/haven/dog/doghaven/Activities/BreedInfoActivity.java
@@ -1,9 +1,6 @@
 package com.haven.dog.doghaven.Activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.ScrollingView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
