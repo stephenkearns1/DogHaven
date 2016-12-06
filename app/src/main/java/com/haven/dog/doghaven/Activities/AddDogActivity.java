9dfb798b5e7069a36e5eb89d8aee8d560cdb85b2
stephenkearns1 <bohsbabycrew1@gmail.com>
2016-12-06 20:21:06 +0000

    Fixed request error with adding dog version 9.0

--- app/src/main/java/com/haven/dog/doghaven/Activities/AddDogActivity.java
+++ app/src/main/java/com/haven/dog/doghaven/Activities/AddDogActivity.java
@@ -31,7 +31,7 @@ public class AddDogActivity extends AppCompatActivity implements View.OnClickLis
     Spinner dropdown;
     private String dname, dbreed, dage;
     private  ProgressDialog pDialog;
-    private final String doghavenAPI_URL = "https://backend-doghaven-app-stephenkearns1.c9users.io/index.php";
+    private final String doghavenAPI_URL = "https://doghaven-backend-app-stephenkearns1.c9users.io/index.php";
 
 
     @Override
@@ -63,6 +63,8 @@ public class AddDogActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
+
+
     public void addDog(){
 
         pDialog = new ProgressDialog(AddDogActivity.this);
@@ -70,7 +72,51 @@ public class AddDogActivity extends AppCompatActivity implements View.OnClickLis
         pDialog.setTitle("Adding New Dog");
         pDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         pDialog.show();
-        StringRequest AddDogRequest = new StringRequest(Request.Method.POST,doghavenAPI_URL,
+        StringRequest AddDogRequest  = new StringRequest(Request.Method.POST,doghavenAPI_URL,
+                new Response.Listener<String>() {
+
+
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        pDialog.hide();
+                        Log.i("Returned data:R01", response);
+                        if(response.equalsIgnoreCase("success")){
+                            //display message account has been created
+
+                            //lanuch the login activity
+
+                        }else{
+                            //display errr message
+                        }
+
+                        ;
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String,String> getParams()  throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("adddog", "adddog");
+                params.put("dname", dname);
+                params.put("dage", dage);
+                params.put("dbreed", dbreed);
+
+                return params;
+            }
+        };
+
+        // Adding the request to the queue
+        MyNetworkingSingletonVolley.getInstance(this).addReuestToQueue(AddDogRequest);
+    }
+
+      /*  StringRequest AddDogRequest = new StringRequest(Request.Method.POST,doghavenAPI_URL,
                 new Response.Listener<String>() {
 
 
@@ -113,6 +159,8 @@ public class AddDogActivity extends AppCompatActivity implements View.OnClickLis
         MyNetworkingSingletonVolley.getInstance(this).addReuestToQueue(AddDogRequest);
 
     }
+
+    */
 
     private void addListenerOnDropDown() {
         dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
