9e0c40fc792e68d3b26bbb78ad45f3dc64b4c007
stephenkearns1 <bohsbabycrew1@gmail.com>
2016-12-18 00:20:17 +0000

    Styling dog match

--- app/src/main/java/com/haven/dog/doghaven/Helpers/MatchingAlogrithm.java
+++ app/src/main/java/com/haven/dog/doghaven/Helpers/MatchingAlogrithm.java
@@ -116,13 +116,27 @@ public class MatchingAlogrithm {
 
             int index = dogScoreList.get(0).getIndexOfDogInList();
             dogsToShow.add((Dog) dogList.get(index));
-        } else{
+        } else if(dogScoreList.size() == 2){
+            for(int i = 0; i < 2; i++){
+
+                int index = dogScoreList.get(i).getIndexOfDogInList();
+                dogsToShow.add((Dog) dogList.get(index));
+            }
+        }else if(dogScoreList.size() == 3){
+            for(int i = 0; i < 3; i++){
+
+                int index = dogScoreList.get(i).getIndexOfDogInList();
+                dogsToShow.add((Dog) dogList.get(index));
+            }
+        }else {
             for(int i = 0; i < 4; i++){
 
                 int index = dogScoreList.get(i).getIndexOfDogInList();
                 dogsToShow.add((Dog) dogList.get(index));
             }
         }
+
+
 
 
 
